f13e3a3294e14b2e4252281f1a60800bb5299d4d
daroOkpeku <stephenjason41@gmail.com>
2021-08-16 09:18:09 +0100

    shopify app update

--- apicode/insertOrder.php
+++ apicode/insertOrder.php
@@ -1,11 +1,16 @@
 <?php
 include("connect.php");
 $total =[];
-$conn = mysqli_connect('localhost', 'root', '', 'test');
-if(mysqli_errno($conn)){
-	echo "failed the connect the DB".mysqli_errno($conn);
-	mysqli_close($conn);
-}
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 $search = "SELECT * FROM shopity";
 $query = mysqli_query($conn, $search);
 $discovery = mysqli_fetch_assoc($query);
