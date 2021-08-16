41e6c91bca0f90b8170c3d1a1efdc1f77cb49cbc
Okpeku stephen <68330153+daroOkpeku@users.noreply.github.com>
2021-08-16 10:21:36 +0100

    shopify app heroku
    shopify app heroku

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "2fc22670e98abe4f39bc94fbac789463";
  $scopes = "read_orders,write_products";
-$redirect_uri = "https://b3fcf07ab611.ngrok.io/dashboard/project/shopity-plug-php-master/token.php";
+$redirect_uri = "https://dellyman-shopify.herokuapp.com/token.php";
 // Build install/approval URL to redirect to
  $install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
 // // Redirect
