d3b923c63bc7b8dcc758595b0fbcba1c9a110d76
Okpeku stephen <68330153+daroOkpeku@users.noreply.github.com>
2021-08-17 11:25:50 +0100

    Update order.php

--- order.php
+++ order.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <?php
-include('./apicode/connect.php');
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 if(!isset($_REQUEST['auth']) && !isset($_REQUEST['id']) && !isset($_REQUEST['shop'])){
   echo "<script> var storage = !!localStorage.getItem('Login')
   let nibble ={};
